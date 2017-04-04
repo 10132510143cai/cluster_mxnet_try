78e62fd2a0a2c73ecc938e253cf53cfaec05489d
10132510143cai <1067694145@qq.com>
2017-04-04 19:52:49 +0800

    init M commit

--- initM.py
+++ initM.py
@@ -27,3 +27,26 @@ def init_m(train_label):
 
     print arrayM
     return arrayM
+
+def init_m_random(train_label):
+    hashmap = {}
+    for i in range(0, train_label.shape[0]):
+        if train_label[i] not in hashmap:
+            hashmap[train_label[i]] = [i]
+        else:
+            keylist = hashmap.get(train_label[i])
+            keylist.append(i)
+            hashmap[train_label[i]] = keylist
+
+    arrayM = np.zeros((train_label.shape[0], train_label.shape[0]))
+
+    for key in hashmap:
+        keylist = hashmap.get(key)
+        for i in range(0, len(keylist)):
+            for j in range(i, len(keylist)):
+                if random.random() <= 0.8:
+                    arrayM[keylist[i]][keylist[j]] = 1
+                    arrayM[keylist[j]][keylist[i]] = 1
+
+    print arrayM
+    return arrayM
